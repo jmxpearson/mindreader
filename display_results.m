3d2929a5b6ee444de3bc4854c1a86fc5084f9d01
John Pearson <jmxpearson@gmail.com>
2014-05-09 13:33:42 -0400

    added scripts to refactor close_trial

---
+++ display_results.m
@@ -0,0 +1,9 @@
+%save trial data and print result here
+% if is_nogo
+%     ttype='No Go';
+% else
+%     ttype='Go';
+% end
+% out_line=sprintf('Trial %d: Type: %s  Result: %s  RT: %0.3g  Correct: %d  Points: %d',...
+%     trialnumdp,ttype,result,rt,correct,pts);
+% disp(out_line)
---
+++ save_data.m
@@ -0,0 +1,9 @@
+%record data
+% data(trialnum).result = result;
+% data(trialnum).onset=onset;
+% data(trialnum).rt=rt;
+% data(trialnum).pts=state.pts;
+% data(trialnum).is_nogo=is_nogo;
+% data(trialnum).correct=correct;
+
+save(fname,'data','pars');
