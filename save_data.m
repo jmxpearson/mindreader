6f85cf44eb2aa840f05a61fb39a46a6529f0f571
John Pearson <jmxpearson@gmail.com>
2014-05-12 09:38:13 -0400

    minor cleanup

--- save_data.m
+++ save_data.m
@@ -1,10 +1,4 @@
 %record data
-% data(trialnum).result = result;
-% data(trialnum).onset=onset;
-% data(trialnum).rt=rt;
-% data(trialnum).pts=state.pts;
-% data(trialnum).is_nogo=is_nogo;
-% data(trialnum).correct=correct;
 data = [data, state];
 
 save(fname,'data','pars');
