cb0443b7ccb31d9999731760ec0e97e84ce88e88
John Pearson <jmxpearson@gmail.com>
2014-05-12 09:40:29 -0400

    updated mark_event codes
    highlighting an option now event 3, input from user now event 4

--- cycle_options.m
+++ cycle_options.m
@@ -8,5 +8,6 @@ if (state.index == 0) || (GetSecs - highlight_on > resp_time)
    end
    state.highlight = state.option_order(state.index);
    
+   mark_event('highlight', plx, 3);
    paint_screen(win_ptr, state);
 end
