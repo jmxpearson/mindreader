7d28b5658d4e942af72e86d9c31c1d71e11f890b
John Pearson <jmxpearson@gmail.com>
2014-05-12 09:43:33 -0400

    updated mark_event to only send nidaq event 1 pulses

--- mark_event.m
+++ mark_event.m
@@ -1,4 +1,10 @@
 function mark_event(eventname, plx, channel, ni)
+% marks an event and timestamp within the state structure for the current
+% trial
+% in addition, sends a TTL pulse to Plexon (if present) on the given
+% channel
+% if Plexon not present, but the NI-DAQ is, send ONLY events on channel 1
+% as 5ms TTL pulses
 
 global state
 
@@ -14,7 +20,7 @@ if plx
     try
         PL_SendUserEvent(plx,channel);
     end
-elseif ni
+elseif ni && (channel == 1)
     PL_DOPulseBit(ni, 1, 5); %send a 5ms TTL pulse through the NIDAQ
 end
 
